a09e97c0d1b37710ffe7fcf67700753c6c2729d3
aiorhiroki <1234defgsigeru@gmail.com>
2019-03-03 13:53:47 +0900

    test with cifar10 with 3 channel images

--- examples/fit_from_directory/cifar10.py
+++ examples/fit_from_directory/cifar10.py
@@ -2,8 +2,8 @@ from farmer.Classifier import Classifier
 from ncc.dataset import prepare_data
 
 num_images_per_class = 100
-module = 'mnist'
+module = 'cifar10'
 
 prepare_data(nb_image=num_images_per_class, module=module, annotation_file=False)
 
-Classifier().fit_from_directory(module)
+Classifier(epochs=10).fit_from_directory(module)

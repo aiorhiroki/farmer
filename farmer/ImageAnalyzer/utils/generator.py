59070fa88b771236da6b6fc2c33b7eeaa12bafe3
aiorhiroki <1234defgsigeru@gmail.com>
2019-05-08 13:11:13 +0900

    read image

--- farmer/ImageAnalyzer/utils/generator.py
+++ farmer/ImageAnalyzer/utils/generator.py
@@ -6,10 +6,18 @@ from .image_util import ImageUtil
 
 
 class ImageSequence(Sequence):
-    def __init__(self, annotations, input_shape, nb_classes, batch_size=1):
+    def __init__(
+        self,
+        annotations,
+        input_shape,
+        nb_classes,
+        task,
+        batch_size=1
+    ):
         self.annotations = annotations
         self.batch_size = batch_size
         self.image_util = ImageUtil(nb_classes, input_shape)
+        self.task = task
 
     def __getitem__(self, idx):
         data = self.annotations[
@@ -18,7 +26,15 @@ class ImageSequence(Sequence):
         batch_x = list()
         batch_y = list()
         for input_file, label in data:
-            batch_x.append(self.image_util.read_image(input_file))
+            batch_x.append(
+                self.image_util.read_image(
+                    input_file, anti_alias=True
+                )
+            )
+            if self.task == 'segmentation':
+                label = self.image_util.read_image(
+                    input_file, normalization=False
+                )
             batch_y.append(label)
 
         return np.array(batch_x), self.image_util.cast_to_onehot(batch_y)

13f6a998b2a532acfe79ba2e694d4faab617a63a
aiorhiroki <1234defgsigeru@gmail.com>
2019-07-23 14:19:10 +0900

    dir_name config

--- config.py
+++ config.py
@@ -1,7 +1,2 @@
-import os
-
-
 class Config(object):
     MILK_API_URL = 'http://127.0.0.1:5000/'
-    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    RESULT_DIR = os.path.join(ROOT_DIR, 'result')

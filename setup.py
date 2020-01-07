dc655fa1b55fc5efa5d65d63f77b700348d772d7
hiroki <1234defgsigeru@gmail.com>
2020-01-07 11:23:20 +0900

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='farmer',
-    version='1.2.5',
+    version='1.2.6',
     description='Auto Machine Learning for Doctor',
     author='Hiroki Matsuzaki',
     author_email='1234defgsigeru@gmail.com',

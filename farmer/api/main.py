066e41b966560203f21538ebb770c6400c304294
aiorhiroki <1234defgsigeru@gmail.com>
2020-01-18 15:05:00 +0900

    inherit run.yaml to each specific yaml

--- farmer/api/main.py
+++ farmer/api/main.py
@@ -8,7 +8,6 @@ from farmer.domain.workflows.train_workflow import TrainWorkflow
 def fit():
     with open("run.yaml") as yamlfile:
         run_config = yaml.safe_load(yamlfile)
-    gpu = run_config.get("gpu")
     config_paths = run_config.get("config_paths")
     if os.path.exists("secret.yaml"):
         with open("secret.yaml") as yamlfile:
@@ -19,7 +18,9 @@ def fit():
         print("config path running: ", config_path)
         with open(config_path) as yamlfile:
             config = yaml.safe_load(yamlfile)
-        config["gpu"] = gpu
+        config.update(
+            {k: v for (k, v) in run_config.items() if k != "config_paths"}
+        )
         if secret_config:
             config.update(secret_config)
         trainer = Trainer(**config)

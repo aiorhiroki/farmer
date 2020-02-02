c1f50d473bf8ea6c991cf94717ed346b624c6ad0
hiroki <1234defgsigeru@gmail.com>
2020-02-02 20:38:01 +0900

    augmentation

--- farmer/ncc/augmentation/__init__.py
+++ farmer/ncc/augmentation/__init__.py
@@ -1,2 +1,4 @@
 from .detection_aug import detection_aug
 from .segmentation_aug import segmentation_aug
+from .augmentation import augmentations
+from .augment_and_mix import augment_and_mix
--- farmer/ncc/augmentation/segmentation_aug.py
+++ farmer/ncc/augmentation/segmentation_aug.py
@@ -1,3 +1,5 @@
+from keras.preprocessing import image
+import numpy as np
 from albumentations import (
     PadIfNeeded,
     HorizontalFlip,
@@ -15,26 +17,39 @@ from albumentations import (
     CLAHE,
     RandomBrightnessContrast,
     RandomGamma,
-    Normalize
+    Blur
 )
 
 
-def segmentation_aug(image, mask, size, augmentation_list):
+def segmentation_aug(input_image, label, size, augmentation_list):
+    if "zoom" in augmentation_list:
+        data_gen_args = dict(zoom_range=[0.75, 1.25])
+        seed = np.random.randint(100)
+        label = np.expand_dims(label, axis=0)
+        label = np.expand_dims(label, axis=-1)
+        image_datagen = image.ImageDataGenerator(**data_gen_args)
+        mask_datagen = image.ImageDataGenerator(**data_gen_args)
+
+        image_datagen.fit(input_image[np.newaxis], augment=True, seed=seed)
+        mask_datagen.fit(label, augment=True, seed=seed)
+
+        image_gen = image_datagen.flow(input_image[np.newaxis], batch_size=1, seed=seed)
+        mask_gen = mask_datagen.flow(label, batch_size=1, seed=seed) 
+
+        gen = zip(image_gen, mask_gen)
+        img_batches, mask_batches = next(gen)
+        input_image = img_batches[0]
+        label = mask_batches[0][..., 0]
+
     height, width = size
     transforms = list()
-    for augmentation in augmentation_list:
-        if augmentation == "HorizontalFlip":
-            transforms.append(HorizontalFlip(p=0.5))
-        elif augmentation == "RandomSizedCrop":
-            transforms.append(
-                RandomSizedCrop(
-                    min_max_height=(0, height//4),
-                    height=height,
-                    width=width,
-                    p=0.5
-                )
-            )
-
-    aug = Compose(transforms, p=1)
-    augmented = aug(image=image, mask=mask)
-    return augmented['image'], augmented['mask']
+    if "vertical_flip":
+        transforms.append(VerticalFlip(p=0.5))
+    if "horizontal_flip":
+        transforms.append(HorizontalFlip(p=0.5))
+    if len(transforms) > 0:
+        aug = Compose(transforms, p=1)
+        augmented = aug(image=input_image ,label=label)
+        return augmented['image'], augmented["label"]
+    else:
+        return input_image, label

fix some bugs about data normalization on PyTorch

farmer/domain/tasks/train_task.py
@@ -1,4 +1,3 @@
-from ncc.utils.image import get_imageset, ImageUtil
 import os
 import numpy as np
 from farmer import ncc
@@ -6,8 +5,6 @@ from tensorflow.python import keras
 
 import torch
 import torch.optim as optim
-import torch.nn as nn
-import torch.nn.functional as F
 import torch.utils.data as data
 import random
 
@@ -401,8 +398,6 @@ class TrainTask:
                     if self.config.cosine_decay:
                         scheduler.step()
 
-                    optimizer.zero_grad()
-
                 else:
                     model.eval()
 
@@ -410,13 +405,13 @@ class TrainTask:
                     if images.size(0) == 1:
                         continue
 
-                    # (mini-batch, height, width, ch) => (mini-batch, ch, height, width)
-                    images = images.permute(0, 3, 1, 2)
-                    labels = labels.permute(0, 3, 1, 2)
+                    images = images.to(device, dtype=torch.float)
+                    labels = labels.to(device, dtype=torch.float)
+                    labels_raw = labels_raw.to(device, dtype=torch.float)
 
-                    images = images.to(device)
-                    labels = labels.to(device)
-                    labels_raw = labels_raw.to(device)
+                    # Due to handle a RuntimeError: 1only batches of spatial targets supported (non-empty 3D tensors)
+                    # but got targets of size: [batch, 1, height, width]
+                    labels_raw = torch.squeeze(labels_raw, dim=1)
 
                     with torch.set_grad_enabled(phase == 'train'):
                         outputs = model(images)
@@ -427,15 +422,14 @@ class TrainTask:
                         acc = criterion_acc(outputs, labels.long())
 
                         if phase == 'train':
+                            optimizer.zero_grad()
                             loss.backward()
+                            optimizer.step()
 
                             epoch_train_loss += loss.item()
                             epoch_train_loss_ce += loss_ce.item()
                             iou_train_list.append(iou.item())
                             acc_train_list.append(acc.item())
-
-                            optimizer.step()
-                            optimizer.zero_grad()
 
                         else:
                             _, predict_result = torch.max(outputs.data, 1)
@@ -472,7 +466,7 @@ class TrainTask:
         df.to_csv('log_output.csv', index=False)
 
         if epochs % self.config.segmentation_val_step != 0:
-            return
+            return model
 
         val_save_dir = os.path.join(self.config.image_path, "validation")
         save_sample_dir = f"{val_save_dir}/epoch_{epoch}"

farmer/ncc/generators/image_dataset.py
@@ -1,18 +1,11 @@
-import math
 import cv2
 
-# from tensorflow.python.keras.utils.data_utils import Sequence
-import numpy as np
 from ..augmentation import segmentation_aug_pytorch
 from ..tasks import Task
 from ..utils import ImageUtil
 
 import torch.utils.data as data
-
-# PyTorch版では、モデルの方に設定するパラメータが含まれている。　
-# Kerasと混同してしまう？ので不要パラメータ削除予定
-# -> LSTM対応やる際、1つのミニバッチで複数読み込む必要がでてくるa
-# -> batch_sizeを削除する予定だったが、撤回して残す。
+from torchvision import transforms
 
 
 class ImageDataset(data.Dataset):
@@ -52,7 +45,6 @@ class ImageDataset(data.Dataset):
             if not ret:
                 return None, None
 
-            input_image = input_image / 255.0
             # (with,height) for cv2.resize
             resize_shape = self.input_shape[::-1]
 
@@ -64,8 +56,10 @@ class ImageDataset(data.Dataset):
                 )
         else:
             input_image = self.image_util.read_image(
-                input_file, anti_alias=True
+                input_file, normalization=False, anti_alias=True
             )
+
+        input_image = transforms.functional.to_tensor(input_image)
 
         if self.task == Task.SEMANTIC_SEGMENTATION:
             label = self.image_util.read_image(
@@ -74,25 +68,20 @@ class ImageDataset(data.Dataset):
                 train_colors=self.train_colors
             )
 
+        label_raw = transforms.functional.to_tensor(label)
+
         if self.augmentation and len(self.augmentation) > 0:
-            input_image, label = segmentation_aug_pytorch(
+            input_image_augmented, label_augmented = segmentation_aug_pytorch(
                 input_image,
-                label,
+                label_raw,
                 self.input_shape,
                 self.augmentation
             )
+        else:
+            input_image_augmented = input_image
+            label_augmented = label_raw
 
-        input_image_result = np.array(input_image, dtype=np.float32)
-        label_result = self.image_util.cast_to_onehot(label)
-        # label_result = label
-
-        # if self.config.framework == 'tensorflow':
-        #     label_result = self.image_util.cast_to_onehot(label)
-
-        # elif self.config.framework == 'pytorch':
-        #     label_result = label
-
-        return input_image_result, label_result, label
+        return input_image_augmented, label_augmented, label_raw
 
     def __len__(self) -> int:
         return len(self.annotations)
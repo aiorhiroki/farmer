55a4f0cbef3adeff9a638433e1dc084f6f783ea6
Atsushi_Yamada <atsushi@vm-nccoly-001.ns5fji2j2diuzekyuw3l5rt45e.lx.internal.cloudapp.net>
2020-04-07 15:57:01 +0900

    add "

--- docker-start.sh
+++ docker-start.sh
@@ -7,7 +7,7 @@ sudo docker run \
 	--mount type=bind,source=/data,target=/data \
 	--mount type=bind,source=/mnt,target=/mnt \
         --mount type=bind,source=/home/$USER/src,target=/home/$USER/src \
-        tensorflow:v2
+        tensorflow:v2 \
 	bash
 
 sudo docker exec -it --user $USER $1 bash --login

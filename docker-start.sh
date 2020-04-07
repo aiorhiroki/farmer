92fee1969302b99790d035d67746552e800e14e3
Atsushi_Yamada <atsushi@vm-nccoly-001.ns5fji2j2diuzekyuw3l5rt45e.lx.internal.cloudapp.net>
2020-04-07 15:32:53 +0900

    sudo docker

--- docker-start.sh
+++ docker-start.sh
@@ -1,11 +1,11 @@
-docker run \
+sudo docker run \
 	--gpus all \
 	-itd \
 	--rm \
 	-p $2:22 \
 	--name $1 \
-	--mount type=bind,source=/mnt,target=/mnt \
-	test:v1 \
+	--mount type=bind,source=/data,target=/data \
+	tensorflow:v2 \
 	bash
 
-docker exec -it --user $USER $1 bash --login
+sudo docker exec -it --user $USER $1 bash --login

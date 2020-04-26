0b31923a3c0577c8e1317bad797cd1c87a0d7a7f
yumion <a.yamada0703@gmail.com>
2020-04-26 23:36:50 +0000

    delete mask of small area

--- farmer/ncc/video/utils.py
+++ farmer/ncc/video/utils.py
@@ -1,5 +1,6 @@
 import cv2
 from timeit import default_timer as timer
+import numpy as np
 
 
 class FPS(object):
@@ -10,6 +11,7 @@ class FPS(object):
                      fps.calculate(draw)
                      cv2.imshow('test', draw)
     """
+
     def __init__(self):
         self.accum_time = 0
         self.curr_fps = 0
@@ -27,7 +29,22 @@ class FPS(object):
             self.fps = "FPS: " + str(self.curr_fps)
             self.curr_fps = 0
         if show:
-            cv2.rectangle(draw, (0,0), (60,20), (255,255,255), -1)
-            cv2.putText(draw, self.fps, (3,13), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0,0,0), 1)
+            cv2.rectangle(draw, (0, 0), (60, 20), (255, 255, 255), -1)
+            cv2.putText(draw, self.fps, (3, 13), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 0), 1)
         else:
             print(self.fps)
+
+
+def delete_small_mask(mask: np.array, threshold: int) -> np.array:
+    gray = mask.copy()
+    _, bw = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    contours, hierarchy = cv2.findContours(bw, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+    if len(contours) == 0:
+        return mask
+
+    for i in range(0, len(contours)):
+        area = cv2.contourArea(contours[i])
+        if area < threshold:
+            cv2.drawContours(gray, contours, i, 0, -1)
+    return gray

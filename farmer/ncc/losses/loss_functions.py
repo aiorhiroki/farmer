01fe746e01b640a56bf28c9053031eb43c4b56e9
Yuto Ishikawa <yishikawa246@gmail.com>
2020-07-06 14:42:14 +0900

    fix loss functions

--- farmer/ncc/losses/loss_functions.py
+++ farmer/ncc/losses/loss_functions.py
@@ -40,19 +40,21 @@ def categorical_crossentropy(class_weights=None, class_indexes=None, **kwargs):
 
 def cce_dice_loss(beta=1, class_weights=None, class_indexes=None, **kwargs):
     loss = categorical_crossentropy(class_weights=class_weights, class_indexes=class_indexes) \
-            + dice_loss(beta=beta)
+            + dice_loss(beta=beta, class_weights=class_weights, class_indexes=class_indexes)
     return loss
 
-def cce_jaccard_loss(**kwargs):
-    loss = categorical_crossentropy() + jaccard_loss()
+def cce_jaccard_loss(class_weights=None, class_indexes=None, per_image=False, **kwargs):
+    loss = categorical_crossentropy(class_weights=class_weights, class_indexes=class_indexes) \
+            + jaccard_loss(class_weights=class_weights, class_indexes=class_indexes, per_image=per_image)
     return loss
 
-def categorical_focal_dice_loss(alpha=0.25, beta=1, gamma=2., **kwargs):
-    loss = categorical_focal_loss(alpha=alpha, gamma=gamma) \
-            + dice_loss(beta=beta)
+def categorical_focal_dice_loss(alpha=0.25, beta=1, gamma=2., class_weights=None, class_indexes=None, **kwargs):
+    loss = categorical_focal_loss(alpha=alpha, gamma=gamma, class_indexes=class_indexes) \
+            + dice_loss(beta=beta, class_weights=class_weights, class_indexes=class_indexes)
     return loss
 
-def categorical_focal_jaccard_loss(alpha=0.25, beta=1, gamma=2., **kwargs):
-    loss = categorical_focal_loss(alpha=alpha, gamma=gamma) \
-            + jaccard_loss()
+def categorical_focal_jaccard_loss(alpha=0.25, gamma=2., class_weights=None,
+                                    class_indexes=None, per_image=False, **kwargs):
+    loss = categorical_focal_loss(alpha=alpha, gamma=gamma, class_indexes=class_indexes) \
+            + jaccard_loss(class_weights=class_weights, class_indexes=class_indexes, per_image=per_image)
     return loss

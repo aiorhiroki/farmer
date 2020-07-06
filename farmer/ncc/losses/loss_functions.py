93c8777710738b1a4c489fa848bb5a7ca9496d42
Yuto <yishikawa246@gmail.com>
2020-07-06 16:25:47 +0900

    make code easier to read

--- farmer/ncc/losses/loss_functions.py
+++ farmer/ncc/losses/loss_functions.py
@@ -33,28 +33,59 @@ def categorical_focal_loss(alpha=0.25, gamma=2., class_indexes=None, **kwargs):
 
 def categorical_crossentropy(class_weights=None, class_indexes=None, **kwargs):
     loss = CategoricalCELoss(
-        class_weights=class_weights, 
+        class_weights=class_weights,
         class_indexes=class_indexes
     )
     return loss
 
 def cce_dice_loss(beta=1, class_weights=None, class_indexes=None, **kwargs):
-    loss = categorical_crossentropy(class_weights=class_weights, class_indexes=class_indexes) \
-            + dice_loss(beta=beta, class_weights=class_weights, class_indexes=class_indexes)
-    return loss
+    cce = categorical_crossentropy(
+        class_weights=class_weights,
+        class_indexes=class_indexes
+    )
+    dl = dice_loss(
+        beta=beta,
+        class_weights=class_weights,
+        class_indexes=class_indexes
+    )
+    return cce + dl
 
 def cce_jaccard_loss(class_weights=None, class_indexes=None, per_image=False, **kwargs):
-    loss = categorical_crossentropy(class_weights=class_weights, class_indexes=class_indexes) \
-            + jaccard_loss(class_weights=class_weights, class_indexes=class_indexes, per_image=per_image)
-    return loss
+    cce = categorical_crossentropy(
+        class_weights=class_weights,
+        class_indexes=class_indexes
+    )
+    jl = jaccard_loss(
+        class_weights=class_weights,
+        class_indexes=class_indexes,
+        per_image=per_image
+    )
+    return cce + jl
 
-def categorical_focal_dice_loss(alpha=0.25, beta=1, gamma=2., class_weights=None, class_indexes=None, **kwargs):
-    loss = categorical_focal_loss(alpha=alpha, gamma=gamma, class_indexes=class_indexes) \
-            + dice_loss(beta=beta, class_weights=class_weights, class_indexes=class_indexes)
-    return loss
+def categorical_focal_dice_loss(alpha=0.25, beta=1, gamma=2.,
+                                class_weights=None, class_indexes=None, **kwargs):
+    cfl = categorical_focal_loss(
+        alpha=alpha,
+        gamma=gamma,
+        class_indexes=class_indexes
+    )
+    dl = dice_loss(
+        beta=beta,
+        class_weights=class_weights,
+        class_indexes=class_indexes
+    )
+    return cfl + dl
 
 def categorical_focal_jaccard_loss(alpha=0.25, gamma=2., class_weights=None,
                                     class_indexes=None, per_image=False, **kwargs):
-    loss = categorical_focal_loss(alpha=alpha, gamma=gamma, class_indexes=class_indexes) \
-            + jaccard_loss(class_weights=class_weights, class_indexes=class_indexes, per_image=per_image)
-    return loss
+    cfl = categorical_focal_loss(
+        alpha=alpha,
+        gamma=gamma,
+        class_indexes=class_indexes
+    )
+    jl = jaccard_loss(
+        class_weights=class_weights,
+        class_indexes=class_indexes,
+        per_image=per_image
+    )
+    return cfl + jl

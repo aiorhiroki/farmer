910469a8644c682ed8f8c32ff0e204177d773868
A.YAMADA <38414956+yumion@users.noreply.github.com>
2020-07-21 14:10:11 +0900

    bug fix

--- farmer/ncc/augmentation/segmentation_aug.py
+++ farmer/ncc/augmentation/segmentation_aug.py
@@ -13,7 +13,7 @@ def segmentation_aug(input_image, label, size, augmentation_list):
         transforms.append(HorizontalFlip(p=0.5))
     if len(transforms) > 0:
         aug = Compose(transforms, p=1)
-        augmented = aug(image=input_image, label=label)
-        return augmented['image'], augmented["label"]
+        augmented = aug(image=input_image, mask=label)
+        return augmented['image'], augmented["mask"]
     else:
         return input_image, label

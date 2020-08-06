0d0726aeab2258d1cb78f975e128a50406abd8f7
sirn524 <sirn524@gmail.com>
2020-08-06 13:17:34 +0900

    work in progress

--- farmer/ncc/augmentation/segmentation_aug.py
+++ farmer/ncc/augmentation/segmentation_aug.py
@@ -1,103 +1,177 @@
 # from keras.preprocessing import image
 import numpy as np
-from skimage import img_as_ubyte
+import albumentations
+from albumentations import Compose
+# from albumentations import (
+#     Compose,
+#     HorizontalFlip,
+#     VerticalFlip,
 
-# def segmentation_aug(input_image, label, size, augmentation_list):
-#     data_gen_args = dict()
-#     if "zoom" in augmentation_list:
-#         data_gen_args["zoom_range"] = [0.5, 1.0]
-#     if "vertical_flip" in augmentation_list:
-#         data_gen_args["vertical_flip"] = True
-#     if "horizontal_flip" in augmentation_list:
-#         data_gen_args["horizontal_flip"] = True
+#     Blur,
+#     CLAHE,
+#     ElasticTransform,
+#     GaussianBlur,
+#     GaussNoise,
+#     GlassBlur,
+#     GridDistortion,
+#     GridDropout,
+#     HueSaturationValue,
+#     Lambda,
+#     MotionBlur,
+#     MedianBlur,
+#     MultiplicativeNoise,
+#     Normalize,
+#     RandomBrightness,
+#     RandomCrop,
+#     RandomContrast,
+#     RandomGamma,
+#     MultiplicativeNoise,
+#     ShiftScaleRotate,
+#     IAAAdditiveGaussianNoise,
+#     IAASharpen,
+#     IAAPerspective,
+#     PadIfNeeded,
 
-#     seed = np.random.randint(100)
-#     label = np.expand_dims(label, axis=0)
-#     label = np.expand_dims(label, axis=-1)
-#     image_datagen = image.ImageDataGenerator(**data_gen_args)
-#     mask_datagen = image.ImageDataGenerator(**data_gen_args)
+#     #used in modules
+#     OneOf,
+#     DualTransform,
+#     # ISONoise,
+# )
 
-#     image_datagen.fit(input_image[np.newaxis], augment=True, seed=seed)
-#     mask_datagen.fit(label, augment=True, seed=seed)
+def round_clip_0_1(x, **kwargs):
+    return x.round().clip(0, 1)
 
-#     image_gen = image_datagen.flow(input_image[np.newaxis], batch_size=1, seed=seed)
-#     mask_gen = mask_datagen.flow(label, batch_size=1, seed=seed)
-
-#     gen = zip(image_gen, mask_gen)
-#     img_batches, mask_batches = next(gen)
-#     input_image = img_batches[0]
-#     label = mask_batches[0][..., 0]
-
-#     return input_image, label
-
-from albumentations import (
-    Compose,
-    HorizontalFlip,
-    VerticalFlip,
-    Blur,
-    MotionBlur,
-    # MedianBlur,
-    GaussianBlur,
-    GlassBlur,
-    # CLAHE,
-    # HueSaturationValue,
-    GaussNoise,
-    Normalize,
-    GridDistortion,
-    RandomContrast,
-    MultiplicativeNoise,
-    GridDropout,
-    ElasticTransform,
-    ISONoise,
-)
-
+def compose_attribute_list():
 
 def segmentation_aug(input_image, label, size, augmentation_list):
     transforms = list()
+    # print(augmentation_list) # ['Gaussiannoise', 'Perspecitve', 'Blur']
+
+    for augmentation_command in augmentation_list:
+        augmentation_command = f"{augmentation_command}()"
+        print(augmentation_command)
+        augmentation= getattr(albumentations, augmentation_command) + '()'   
+        print(augmentation)
+        # augmentation = augmentation + '()'
+        transforms.append(augmentation)
+        print(transforms)
+        print(transforms[0])
+
+        augmentation= getattr(albumentations, augmentation_command)    
+        print(augmentation)
+        print(transforms)
+    print(transforms)
+        if type(augmentation_command) == list:
+            continue
+            # transforms.append(OneOf(getattr(albumentations,str(augmentation_command)for augmentation_ in augmentation_command)))
+        else:
+            augmentation_command= getattr(albumentations, augmentation_command)
+            transforms.append(augmentation_command)
+            print(transforms)
+            print(augmentation_command)
+            while augmentation_command ==list:
+                augmentation_command = getattr()
+        print(augmentation_command)
+        augmentation_command = getattr(albumentations, augmentation_command)
+        
+
+
+# def segmentation_aug(input_image, label, size, augmentation_list):
+#     transforms = list()
+
+#     for augmentation_command in augmentation_list:
+#         if type(augmentation_command) == list:
+#             augmentation_command = f"OneOf({augmentation_command},p=0.9)"
+#         else:
+#             augmentation_command = augmentation_command    
+#     # print(size)
+#     height, width = size
+#     # print(height)
+#     # print(width)
+#     # transforms.append(
+#     #     getattr(albu,augmentation_command)(
+#     #     **self.config.augmentation_command
+#     #     )
+#     # )
+#     # print(transforms)
+
+#     if "vertical_flip" in augmentation_command:
+#         transforms.append(VerticalFlip(p=0.5))
+#     if "HorizontalFlip" in augmentation_command:
+#         transforms.append(HorizontalFlip(p=0.5))
+#     # if "blur" in augmentation_command:
+#     #     transforms.append(Blur(blur_limit=3,p=0.5))
+#     #newly added
+#     if "motion_blur" in augmentation_command:
+#         transforms.append(MotionBlur(blur_limit=3,p=0.5))
+#     if "median_blur" in augmentation_command:
+#         transforms.append(MedianBlur(blur_limit=3,p=0.5))
+#     if "gaussian_blur" in augmentation_command:
+#         transforms.append(GaussianBlur(blur_limit=3,p=0.5))
+#     if "glass_blur" in augmentation_command:
+#         transforms.append(GlassBlur(sigma=0.7, max_delta=4, iterations=2,p=0.5))
+#     if "gauss_noise" in augmentation_command:
+#         transforms.append(GaussNoise(p=0.5))
+#     if "normalize" in augmentation_command:
+#         transforms.append(Normalize(p=0.5))
+#     if "grid_distortion" in augmentation_command:
+#         transforms.append(GridDistortion
+#             (num_steps=5, distort_limit=0.3, interpolation=1, border_mode=4, p=0.5)
+#             )
+#     if "RandomContrast" in augmentation_command:
+#         transforms.append(RandomContrast(limit=0.2, p=0.5))   
+#     if "multiplicative_noise" in augmentation_command:
+#         transforms.append(MultiplicativeNoise(multiplier=(0.9, 1.1), per_channel=False, elementwise=False, p=0.5))   
+#     if "grid_dropout" in augmentation_command:
+#         transforms.append(GridDropout(p=0.5))            
+#     if "elastic_transform" in augmentation_command:
+#         transforms.append(ElasticTransform(alpha=1, sigma=50, alpha_affine=50, interpolation=1, border_mode=4, p=0.5))
+#     if "isonoise" in augmentation_command:
+#         transforms.append(ISONoise(color_shift=(0.01, 0.05), intensity=(0.1, 0.5), p=0.5))    
+#     if "CLAHE" in augmentation_command:
+#         transforms.append(CLAHE(p=1))
+#     if "glass_blur" in augmentation_command:
+#         transforms.append(GlassBlur(sigma=0.7, max_delta=4, iterations=2, p=0.5))
     
-    label = img_as_ubyte(label)
-    if "vertical_flip" in augmentation_list:
-        transforms.append(VerticalFlip(p=0.5))
-    if "horizontal_flip" in augmentation_list:
-        transforms.append(HorizontalFlip(p=0.5))
-    if "blur" in augmentation_list:
-        transforms.append(Blur(blur_limit=3,p=0.5))
-    #newly added
-    if "motion_blur" in augmentation_list:
-        transforms.append(MotionBlur(blur_limit=3,p=0.5))
-    if "median_blur" in augmentation_list:
-        transforms.append(MedianBlur(blur_limit=3,p=0.5))
-    if "gaussian_blur" in augmentation_list:
-        transforms.append(GaussianBlur(blur_limit=3,p=0.5))
-    if "glass_blur" in augmentation_list:
-        transforms.append(GlassBlur(sigma=0.7, max_delta=4, iterations=2,p=0.5))
-    if "gauss_noise" in augmentation_list:
-        transforms.append(GaussNoise(p=0.5))
-    if "normalize" in augmentation_list:
-        transforms.append(Normalize(p=0.5))
-    if "grid_distortion" in augmentation_list:
-        transforms.append(GridDistortion
-            (num_steps=5, distort_limit=0.3, interpolation=1, border_mode=4, p=0.5)
-            )
-    if "random_contrast" in augmentation_list:
-        transforms.append(RandomContrast(limit=0.2, p=0.5))   
-    if "multiplicative_noise" in augmentation_list:
-        transforms.append(MultiplicativeNoise(multiplier=(0.9, 1.1), per_channel=False, elementwise=False, p=0.5))   
-    if "grid_dropout" in augmentation_list:
-        transforms.append(GridDropout(p=0.5))            
-    if "elastic_transform" in augmentation_list:
-        transforms.append(ElasticTransform(alpha=1, sigma=50, alpha_affine=50, interpolation=1, border_mode=4, p=0.5))
-    if "isonoise" in augmentation_list:
-        transforms.append(ISONoise(color_shift=(0.01, 0.05), intensity=(0.1, 0.5), p=0.5))    
-   # if "clahe" in augmentation_list:
-    #     transforms.append(CLAHE(p=0.5))
-    # if "hsv" in augmentation_list:
-    #     transforms.append(HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20,p=0.5))
+#     #Module:happy-set
+#     if "ShiftScaleRotate" in augmentation_command:
+#         transforms.append(ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, border_mode=0, p=1))
+    
+#     if "PadIfNeeded" in augmentation_command:
+#         transforms.append(PadIfNeeded(min_height=256, min_width=512, always_apply=True, border_mode=0))
+#     if "RandomCrop" in augmentation_command:
+#         transforms.append(RandomCrop(height=256, width=512, always_apply=True))
+    
+#     if "IAAAdditiveGaussianNoise" in augmentation_command:
+#         transforms.append(IAAAdditiveGaussianNoise(p=0.2))
+#     if "IAAPerspective" in augmentation_command:
+#         transforms.append(IAAPerspective(p=0.5))
+
+#     if "RandomBrightness" in augmentation_command:
+#         transforms.append(RandomBrightness(p=1))
+#     if "RandomGamma" in augmentation_command:
+#         transforms.append(RandomGamma(p=1))
+#     if "IAASharpen" in augmentation_command:
+#         transforms.append(IAASharpen(p=1))
+#     if "Blur" in augmentation_command:
+#         transforms.append(Blur(blur_limit=3,p=1))
+#     if "MotionBlur" in augmentation_command:
+#         transforms.append(MotionBlur(blur_limit=3,p=1))
+
+#     if "RandomContrast" in augmentation_command:
+#         transforms.append(RandomContrast(limit=0.2, p=1))   
+#     if "HueSaturationValue" in augmentation_command:
+#         transforms.append(HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=1))
+#     if "Lambda" in augmentation_command:
+#         transforms.append(Lambda(mask=round_clip_0_1))
 
     if len(transforms) > 0:
-        print('augmentation!!!')
+        # print(transforms[0])
         aug = Compose(transforms, p=1)
+        # print(transforms)
+        # print(type(transforms[0]))
         augmented = aug(image=input_image, mask=label)
         return augmented['image'], augmented["mask"]
     else:
         return input_image, label
+

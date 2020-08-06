c3a8e4f48287528cb4bbb5ad25127c2527455d0c
yumion <a.yamada0703@gmail.com>
2020-08-06 14:52:34 +0900

    debug and get aug params

--- farmer/ncc/augmentation/segmentation_aug.py
+++ farmer/ncc/augmentation/segmentation_aug.py
@@ -38,8 +38,10 @@ from albumentations import Compose
 #     # ISONoise,
 # )
 
+
 def round_clip_0_1(x, **kwargs):
     return x.round().clip(0, 1)
+
 
 def segmentation_aug(input_image, label, size, augmentation_list):
     transforms = list()
@@ -49,15 +51,21 @@ def segmentation_aug(input_image, label, size, augmentation_list):
         # augmentation= f"{augmentation_command}()"
         print(augmentation_command)
 
-        augmentation= getattr(albumentations, augmentation_command) #+ '()'   
+        if type(augmentation_command) == str:
+            print('str')
+            augmentation = getattr(albumentations, augmentation_command)()
+        elif type(augmentation_command) == dict:
+            print('dict')
+            print(list(augmentation_command.keys()), list(augmentation_command.values()))
+            augmentation = getattr(albumentations, list(augmentation_command.keys())[0])(**list(augmentation_command.values())[0])
+
         print(augmentation)
         # augmentation = augmentation + '()'
         # transforms.append(augmentation)
         transforms.append(augmentation)
         print(transforms)
-        print(transforms[0])
 
-    #     augmentation= getattr(albumentations, augmentation_command)    
+    #     augmentation= getattr(albumentations, augmentation_command)
     #     print(augmentation)
     #     print(transforms)
     # print(transforms)
@@ -81,7 +89,7 @@ def segmentation_aug(input_image, label, size, augmentation_list):
 #         if type(augmentation_command) == list:
 #             augmentation_command = f"OneOf({augmentation_command},p=0.9)"
 #         else:
-#             augmentation_command = augmentation_command    
+#             augmentation_command = augmentation_command
 #     # print(size)
 #     height, width = size
 #     # print(height)
@@ -117,29 +125,29 @@ def segmentation_aug(input_image, label, size, augmentation_list):
 #             (num_steps=5, distort_limit=0.3, interpolation=1, border_mode=4, p=0.5)
 #             )
 #     if "RandomContrast" in augmentation_command:
-#         transforms.append(RandomContrast(limit=0.2, p=0.5))   
+#         transforms.append(RandomContrast(limit=0.2, p=0.5))
 #     if "multiplicative_noise" in augmentation_command:
-#         transforms.append(MultiplicativeNoise(multiplier=(0.9, 1.1), per_channel=False, elementwise=False, p=0.5))   
+#         transforms.append(MultiplicativeNoise(multiplier=(0.9, 1.1), per_channel=False, elementwise=False, p=0.5))
 #     if "grid_dropout" in augmentation_command:
-#         transforms.append(GridDropout(p=0.5))            
+#         transforms.append(GridDropout(p=0.5))
 #     if "elastic_transform" in augmentation_command:
 #         transforms.append(ElasticTransform(alpha=1, sigma=50, alpha_affine=50, interpolation=1, border_mode=4, p=0.5))
 #     if "isonoise" in augmentation_command:
-#         transforms.append(ISONoise(color_shift=(0.01, 0.05), intensity=(0.1, 0.5), p=0.5))    
+#         transforms.append(ISONoise(color_shift=(0.01, 0.05), intensity=(0.1, 0.5), p=0.5))
 #     if "CLAHE" in augmentation_command:
 #         transforms.append(CLAHE(p=1))
 #     if "glass_blur" in augmentation_command:
 #         transforms.append(GlassBlur(sigma=0.7, max_delta=4, iterations=2, p=0.5))
-    
+
 #     #Module:happy-set
 #     if "ShiftScaleRotate" in augmentation_command:
 #         transforms.append(ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, border_mode=0, p=1))
-    
+
 #     if "PadIfNeeded" in augmentation_command:
 #         transforms.append(PadIfNeeded(min_height=256, min_width=512, always_apply=True, border_mode=0))
 #     if "RandomCrop" in augmentation_command:
 #         transforms.append(RandomCrop(height=256, width=512, always_apply=True))
-    
+
 #     if "IAAAdditiveGaussianNoise" in augmentation_command:
 #         transforms.append(IAAAdditiveGaussianNoise(p=0.2))
 #     if "IAAPerspective" in augmentation_command:
@@ -157,7 +165,7 @@ def segmentation_aug(input_image, label, size, augmentation_list):
 #         transforms.append(MotionBlur(blur_limit=3,p=1))
 
 #     if "RandomContrast" in augmentation_command:
-#         transforms.append(RandomContrast(limit=0.2, p=1))   
+#         transforms.append(RandomContrast(limit=0.2, p=1))
 #     if "HueSaturationValue" in augmentation_command:
 #         transforms.append(HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=1))
 #     if "Lambda" in augmentation_command:
@@ -172,4 +180,3 @@ def segmentation_aug(input_image, label, size, augmentation_list):
         return augmented['image'], augmented["mask"]
     else:
         return input_image, label
-

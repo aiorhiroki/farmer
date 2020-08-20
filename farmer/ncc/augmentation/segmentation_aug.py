4c266b067ca026b8753cd2e96d1cd7ca03aa24a1
sirn524 <sirn524@outlook.jp>
2020-08-20 18:05:54 +0900

    albumentation function added

--- farmer/ncc/augmentation/segmentation_aug.py
+++ farmer/ncc/augmentation/segmentation_aug.py
@@ -1,6 +1,39 @@
 from keras.preprocessing import image
 import numpy as np
 from .augment_and_mix import augment_and_mix
+import albumentations
+
+def segmentation_alb(input_image, label, mean, std, augmentation_dict):
+    transforms = get_aug(augmentation_dict)
+    width = input_image.shape[1]
+
+    if len(transforms) > 0:
+        aug = albumentations.Compose(transforms, p=1)
+        augmented = aug(image=input_image, mask=label)
+        return augmented['image'], augmented["mask"]
+
+    else:
+        return input_image, label
+
+def get_aug(augmentation_dict):
+    transforms = list()
+    for aug_command, aug_param in augmentation_dict.items():
+        print(aug_param)
+        if aug_command.startswith("OneOf"):
+            augs = get_aug(aug_param)
+            augmentation = albumentations.OneOf(augs, aug_param['p'])
+            transforms.append(augmentation)
+        elif aug_command == 'p':
+            continue
+        else:
+            if aug_param is None:
+                augmentation = getattr(albumentations, aug_command)()
+            else:
+                augmentation = getattr(albumentations, aug_command)(aug_param)
+                print(aug_param)
+            transforms.append(augmentation)
+    return transforms
+
 
 
 def segmentation_aug(input_image, label, mean, std, augmentation_dict):

4d4ad959a925f6edcd21ff62ed16bc8574238149
hiroki <1234defgsigeru@gmail.com>
2020-09-01 14:25:40 +0900

    delete dataloader

--- farmer/ncc/generators/__init__.py
+++ farmer/ncc/generators/__init__.py
@@ -1,4 +1,4 @@
 from .image_sequence import ImageSequence
 from .generate_with_mask import generate_with_mask
-from .dataloader import Dataloder, gen
+from .dataloader import gen
 from .dataset import ClassificationDataset, SegmentationDataset

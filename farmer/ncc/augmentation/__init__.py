0c6fdff47525cbc01eb7264765464085a1b3736d
sirn524 <sirn524@outlook.jp>
2020-09-14 15:13:54 +0900

    erased segmentation_aug module for modification

--- farmer/ncc/augmentation/__init__.py
+++ farmer/ncc/augmentation/__init__.py
@@ -1,4 +1,4 @@
 from .detection_aug import detection_aug
-from .segmentation_aug import segmentation_aug, segmentation_alb
+from .segmentation_aug import segmentation_alb
 from .augmentation import augmentations
 from .augment_and_mix import augment_and_mix

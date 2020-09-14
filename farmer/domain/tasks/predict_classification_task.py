83174a508f8b1846dc4da659c72c33fe487cb696
hiroki <1234defgsigeru@gmail.com>
2020-09-14 09:36:46 +0900

    input_data_type video or image

--- farmer/domain/tasks/predict_classification_task.py
+++ farmer/domain/tasks/predict_classification_task.py
@@ -43,21 +43,28 @@ class PredictClassificationTask:
         if save_npy:
             np.save(f"{self.config.info_path}/pred.npy", prediction)
 
-        class_count = np.array([28666,  2727,   671,   813,   603,   432])
-        class_weight = np.sum(class_count)/(len(class_count)*class_count)
-        prediction *= class_weight
         prediction_classes = np.argmax(prediction, axis=-1)
         pred_result = list()
-        for files, prediction_cls in zip(annotation_set, prediction_classes):
-            image_file, frame_id, true_cls = files
-            pred_result.append(
-                [
-                    image_file,
-                    frame_id,
-                    self.config.class_names[int(prediction_cls)],
-                    self.config.class_names[int(true_cls)]
-                ]
-            )
+        for files, pred_cls in zip(annotation_set, prediction_classes):
+            if self.config.input_data_type == "video":
+                image_file, frame_id, true_cls = files
+                pred_result.append(
+                    [
+                        image_file,
+                        frame_id,
+                        self.config.class_names[int(pred_cls)],
+                        self.config.class_names[int(true_cls)]
+                    ]
+                )
+            else:
+                image_file, true_cls = files
+                pred_result.append(
+                    [
+                        image_file,
+                        self.config.class_names[int(pred_cls)],
+                        self.config.class_names[int(true_cls)]
+                    ]
+                )
         with open(f"{self.config.info_path}/pred.csv", "w") as fw:
             writer = csv.writer(fw)
             writer.writerows(pred_result)

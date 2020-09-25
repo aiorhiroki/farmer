2406585b4351cf3784e14e1469e32bfdc605da03
Yuto Ishikawa <yishikawa246@gmail.com>
2020-09-25 19:02:27 +0900

    make each scheduler class

--- farmer/domain/model/scheduler_model.py
+++ farmer/domain/model/scheduler_model.py
@@ -13,3 +13,23 @@ class LRScheduler:
     exp_gamma: float = 0.95
     cyc_lr_max: float = 0.01
     cyc_lr_min: float = 0.001
+
+class StepLR:
+    step_size: int = None
+    step_gamma: float = None
+
+class MultiStepLR:
+    step_gamma: float = 0.5
+    milestones: dict = None
+
+class ExponentialLR:
+    exp_gamma: float = 0.90
+
+class CyclicalLR:
+    step_size: int = None
+    cyc_lr_max: float = None
+    cyc_lr_min: float = None
+
+class CosineDecay:
+    cosine_lr_max: float = 0.001
+    cosine_lr_min: float = 0.0001

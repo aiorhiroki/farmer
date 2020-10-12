fc4aff6e164443078e0b8e03b19e0bdef2c00789
Yuto Ishikawa <yishikawa246@gmail.com>
2020-10-12 09:18:42 +0900

    レビュー指摘修正

--- farmer/domain/tasks/eda_task.py
+++ farmer/domain/tasks/eda_task.py
@@ -7,6 +7,7 @@ import cv2
 import random
 from tqdm import trange
 
+
 class EdaTask:
     def __init__(self, config):
         self.config = config
@@ -68,23 +69,22 @@ class EdaTask:
         if self.config.input_data_type == 'image' and self.config.mean_std:
             if len(train_set) == 0:
                 return
-        else:
-            return
-        if len(self.config.mean) > 0 and len(self.config.std) > 0:
-            return
-        means = []
-        pix_pow = np.zeros(3)
-        for i in trange(len(train_set)):
-            input_file, label = train_set[i]
-            x = cv2.imread(input_file)
-            x = cv2.resize(x, (self.config.width, self.config.height))
-            x = x / 255.  # 正規化してからmean,stdを計算する
-            means.append(np.mean(x, axis=(0, 1)))
-            pix_pow += np.sum(np.power(x, 2), axis=(0, 1))
-        pix_num = self.config.height *  self.config.width * len(train_set)
-        mean = np.mean(means, axis=(0))
-        var_pix = (pix_pow / pix_num) - np.power(mean, 2) 
-        std = np.sqrt(var_pix)
-        # convert BGR to RGB
-        self.config.mean = mean[::-1]
-        self.config.std = std[::-1]
+            elif len(self.config.mean) > 0 and len(self.config.std) > 0:
+                return
+
+            means = []
+            pix_pow = np.zeros(3)
+            for i in trange(len(train_set)):
+                input_file, label = train_set[i]
+                x = cv2.imread(input_file)
+                x = cv2.resize(x, (self.config.width, self.config.height))
+                x = x / 255.  # 正規化してからmean,stdを計算する
+                means.append(np.mean(x, axis=(0, 1)))
+                pix_pow += np.sum(np.power(x, 2), axis=(0, 1))
+            pix_num = self.config.height *  self.config.width * len(train_set)
+            mean = np.mean(means, axis=(0))
+            var_pix = (pix_pow / pix_num) - np.power(mean, 2)
+            std = np.sqrt(var_pix)
+            # convert BGR to RGB
+            self.config.mean = mean[::-1]
+            self.config.std = std[::-1]

338dc7c2b75b44f49cc09330af6fd5dc0d600039
sirn524 <sirn524@outlook.jp>
2020-10-29 16:12:20 +0900

    classification deleted

--- farmer/ncc/augmentation/segmentation_aug.py
+++ farmer/ncc/augmentation/segmentation_aug.py
@@ -6,7 +6,7 @@ import albumentations
 
 def segmentation_aug(
         input_image,
-        label, 
+        label,
         mean,
         std,
         augmentation_dict,
@@ -23,33 +23,6 @@ def segmentation_aug(
                 augmented['mask'],
                 transforms,
                 mean, std
-            )
-        return augmented['image'], augmented["mask"]
-
-    else:
-        return input_image, label
-
-
-def classification_aug(
-        input_image,
-        label, 
-        mean,
-        std,
-        augmentation_dict,
-        augmix,
-):
-    transforms = get_aug(augmentation_dict)
-
-    if len(transforms) > 0:
-        aug = albumentations.Compose(transforms, p=1)
-        augmented = aug(image=input_image, mask=label)
-        if augmix:
-            augmented['image'] = augment_and_mix(
-                augmented['image'],
-                augmented["mask"],
-                transforms,
-                mean, 
-                std
             )
         return augmented['image'], augmented["mask"]
 

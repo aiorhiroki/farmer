dcfc066f21ba109559354cfeb5594ae35c387a53
sirn524 <sirn524@outlook.jp>
2020-10-29 16:11:32 +0900

    init changed

--- farmer/ncc/augmentation/__init__.py
+++ farmer/ncc/augmentation/__init__.py
@@ -1,4 +1,5 @@
 from .detection_aug import detection_aug
-from .segmentation_aug import segmentation_aug, classification_aug
+from .segmentation_aug import segmentation_aug
+from .classification_aug import classification_aug
 from .augmentation import augmentations
 from .augment_and_mix import augment_and_mix

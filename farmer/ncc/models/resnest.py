23d95ab2dd369390727b8a1454e1315b2189d076
yumion <a.yamada0703@gmail.com>
2020-11-24 23:04:13 +0000

    add include_top

--- farmer/ncc/models/resnest.py
+++ farmer/ncc/models/resnest.py
@@ -1,3 +1,7 @@
+"""from this repository
+https://github.com/QiaoranC/tf_ResNeSt_RegNet_model
+"""
+
 import tensorflow as tf
 # tf.enable_eager_execution()
 # tf.compat.v1.enable_eager_execution()
@@ -105,7 +109,7 @@ class ResNest:
     def __init__(self, verbose=False, input_shape=(224, 224, 3), active="relu", nb_classes=81,
                  dropout_rate=0.2, fc_activation=None, blocks_set=[3, 4, 6, 3], radix=2, groups=1,
                  bottleneck_width=64, deep_stem=True, stem_width=32, block_expansion=4, avg_down=True,
-                 avd=True, avd_first=False, preact=False, using_basic_block=False, using_cb=False):
+                 avd=True, avd_first=False, preact=False, using_basic_block=False, using_cb=False, include_top=True):
         self.channel_axis = -1  # not for change
         self.verbose = verbose
         self.active = active  # default relu
@@ -131,6 +135,7 @@ class ResNest:
         self.preact = preact
         self.using_basic_block = using_basic_block
         self.using_cb = using_cb
+        self.include_top = include_top
 
     def _make_stem(self, input_tensor, stem_width=64, deep_stem=False):
         x = input_tensor
@@ -189,10 +194,8 @@ class ResNest:
         else:
             gap = x
 
-        # print('sum',gap.shape)
         gap = GlobalAveragePooling2D(data_format="channels_last")(gap)
         gap = tf.reshape(gap, [-1, 1, 1, filters])
-        # print('adaptive_avg_pool2d',gap.shape)
 
         reduction_factor = 4
         inter_channels = max(in_channels * radix // reduction_factor, 32)
@@ -263,7 +266,7 @@ class ResNest:
 
         if avd and not avd_first:
             x = avd_layer(x)
-            # print('can')
+
         x = Conv2D(filters * self.block_expansion, kernel_size=1, strides=1, padding="same", kernel_initializer="he_normal",
                    dilation_rate=self.dilation, use_bias=False, data_format="channels_last")(x)
         x = BatchNormalization(axis=self.channel_axis, epsilon=1.001e-5)(x)
@@ -317,7 +320,6 @@ class ResNest:
 
         if avd and not avd_first:
             x = avd_layer(x)
-            # print('can')
 
         x = BatchNormalization(axis=self.channel_axis, epsilon=1.001e-5)(x)
         x = Activation(self.active)(x)
@@ -331,24 +333,20 @@ class ResNest:
         if self.using_basic_block is True:
             x = self._make_block_basic(x, first_block=True, filters=filters, stride=stride, radix=self.radix,
                                        avd=self.avd, avd_first=self.avd_first, is_first=is_first)
-            # print('0',x.shape)
 
             for i in range(1, blocks):
                 x = self._make_block_basic(
                     x, first_block=False, filters=filters, stride=1, radix=self.radix, avd=self.avd, avd_first=self.avd_first
                 )
-                # print(i,x.shape)
 
         elif self.using_basic_block is False:
             x = self._make_block(x, first_block=True, filters=filters, stride=stride, radix=self.radix, avd=self.avd,
                                  avd_first=self.avd_first, is_first=is_first)
-            # print('0',x.shape)
 
             for i in range(1, blocks):
                 x = self._make_block(
                     x, first_block=False, filters=filters, stride=1, radix=self.radix, avd=self.avd, avd_first=self.avd_first
                 )
-                # print(i,x.shape)
         return x
 
     def _make_Composite_layer(self, input_tensor, filters=256, kernel_size=1, stride=1, upsample=True):
@@ -403,24 +401,25 @@ class ResNest:
             if self.verbose:
                 print('----- layer {} out {} -----'.format(idx, x.shape))
 
-        x = GlobalAveragePooling2D(name='avg_pool')(x)
+        if self.include_top:
+            x = GlobalAveragePooling2D(name='avg_pool')(x)
+            if self.verbose:
+                print("pool_out:", x.shape)  # remove the concats var
+
+            if self.dropout_rate > 0:
+                x = Dropout(self.dropout_rate, noise_shape=None)(x)
+
+            x = Dense(self.nb_classes, kernel_initializer="he_normal", use_bias=False, name="fc_NObias")(x)  # replace concats to x
+            if self.verbose:
+                print("fc_out:", x.shape)
+
+            if self.fc_activation:
+                x = Activation(self.fc_activation)(x)
+
+        model = models.Model(inputs=input_sig, outputs=x)
+
         if self.verbose:
-            print("pool_out:", x.shape)  # remove the concats var
-
-        if self.dropout_rate > 0:
-            x = Dropout(self.dropout_rate, noise_shape=None)(x)
-
-        fc_out = Dense(self.nb_classes, kernel_initializer="he_normal", use_bias=False, name="fc_NObias")(x)  # replace concats to x
-        if self.verbose:
-            print("fc_out:", fc_out.shape)
-
-        if self.fc_activation:
-            fc_out = Activation(self.fc_activation)(fc_out)
-
-        model = models.Model(inputs=input_sig, outputs=fc_out)
-
-        if self.verbose:
-            print("Resnest builded with input {}, output{}".format(input_sig.shape, fc_out.shape))
+            print("Resnest builded with input {}, output{}".format(input_sig.shape, x.shape))
             print("-------------------------------------------")
             print("")
 
@@ -428,10 +427,10 @@ class ResNest:
 
 
 def resnest(model_name='ResNest50', height=224, width=224, nb_classes=81,
-            verbose=False, dropout_rate=0, fc_activation=None, **kwargs):
+            verbose=False, dropout_rate=0, fc_activation='softmax', include_top=True, **kwargs):
     '''fetch_resnest
     input_shape: (h,w,c)
-    fc_activation: sigmoid,softmax
+    fc_activation: sigmoid, softmax
     '''
     model_name = model_name.lower()
 
@@ -461,7 +460,7 @@ def resnest(model_name='ResNest50', height=224, width=224, nb_classes=81,
             blocks_set=resnest_parameters[model_name]['blocks_set'],
             radix=2, groups=1, bottleneck_width=64, deep_stem=True,
             stem_width=resnest_parameters[model_name]['stem_width'],
-            avg_down=True, avd=True, avd_first=False, **kwargs
+            avg_down=True, avd=True, avd_first=False, include_top=include_top, **kwargs
         ).build()
 
     return model

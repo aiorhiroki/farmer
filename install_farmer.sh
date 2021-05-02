3e92f127292d3ba2ebfd49123cdb4849495d5e23
aiorhiroki <1234defgsigeru@gmail.com>
2021-05-02 17:45:14 +0900

    revert setting file

--- install_farmer.sh
+++ install_farmer.sh
@@ -1,4 +1,4 @@
-docker exec -it farmer_for_detection bash -c \
+docker exec -it farmer bash -c \
     "cd $PWD && \
     poetry run python setup.py develop && \
     echo $PWD, $(date), $(id -u -n) >> ~/.farmerpath.csv"

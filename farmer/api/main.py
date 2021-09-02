b9506b43d63f2a67d3958de63e152e0e064b627c
yHarai <yuhar2@ncc.go.jp>
2021-09-02 10:34:41 +0900

    artifactsで保存する処理を変更。example/yamlをmlflow版に変更。

--- farmer/api/main.py
+++ farmer/api/main.py
@@ -12,7 +12,7 @@ from farmer.ncc.utils import cross_val_split
 from farmer.domain.model.task_model import Task
 from farmer.domain.model import Trainer
 from farmer.domain.workflows.train_workflow import TrainWorkflow
-
+from farmer.ncc.mlflow_wrapper.mlflow_client_wrapper import MlflowClientWrapper
 
 def fit():
     yaml.add_constructor(
@@ -41,6 +41,13 @@ def fit():
             config.update(secret_config)
         trainer = Trainer(**config)
         val_dirs = trainer.val_dirs
+        
+        if trainer.mlflow:
+            client = MlflowClientWrapper.create_run(experiment_name=trainer.experiment_name,
+                                                    run_name=trainer.run_name,
+                                                    user_name=trainer.user_name)
+            trainer.artifacts_path = client.get_artifacts_path()
+        
         if trainer.training and (val_dirs is None or len(val_dirs) == 0):
             # cross validation
             if trainer.task == Task.SEMANTIC_SEGMENTATION:
--- farmer/domain/tasks/output_result_task.py
+++ farmer/domain/tasks/output_result_task.py
@@ -1,5 +1,6 @@
 import yaml
-
+from farmer.ncc.tasks import Task
+from farmer.ncc.mlflow_wrapper.mlflow_client_wrapper import MlflowClientWrapper
 
 class OutputResultTask:
     def __init__(self, config):
@@ -7,6 +8,14 @@ class OutputResultTask:
 
     def command(self, result, trial=None):
         self._do_write_result_task(result, trial)
+
+        if self.config.mlflow:
+            if not MlflowClientWrapper.is_running():
+                MlflowClientWrapper.create_run(experiment_name=self.config.experiment_name,
+                                                run_name=self.config.run_name,
+                                                user_name=self.config.user_name)
+            self._do_log_mlflow_task()
+            MlflowClientWrapper.end_run()
 
     def _do_write_result_task(self, result, trial):
         self.config.result = result
@@ -21,3 +30,45 @@ class OutputResultTask:
                 configfile.write(f"\n optuna trial#{trial_number}")
                 configfile.write(
                     f"\n optuna params = {self.config.trial_params}")
+
+    def _do_log_mlflow_task(self):
+        print('[I] OutputResultTask._do_log_mlflow_task')
+        
+        MlflowClientWrapper.log_params({
+            "model_name": self.config.train_params.model_name,
+            "backbone": self.config.train_params.backbone,
+            "activation": self.config.train_params.activation,
+            "optimizer": self.config.train_params.optimizer,
+            "class_names": self.config.class_names,
+            "epochs": self.config.epochs,
+            "loss": self.config.train_params.loss.get('functions'),
+            "batch_size": self.config.train_params.batch_size,
+            "learning_rate": self.config.train_params.learning_rate,
+            "nb_train_data": self.config.nb_train_data,
+            "nb_validation_data": self.config.nb_validation_data,
+            "nb_test_data": self.config.nb_test_data,
+        })
+
+        MlflowClientWrapper.set_tags({
+            "mlflow.note.content": self.config.description,
+            "version": self.config.version
+        })
+        
+        if self.config.optuna:
+            MlflowClientWrapper.set_tags({
+                "optuna trial": f"trial#{self.config.trial_number}"
+            })
+        
+        MlflowClientWrapper.save_artifacts_to_mlruns(self.config.info_path, artifact_dir_name="info")
+        MlflowClientWrapper.save_artifacts_to_mlruns(self.config.learning_path, artifact_dir_name="learning")
+        MlflowClientWrapper.save_artifacts_to_mlruns(self.config.model_path, artifact_dir_name="model")
+        
+        if self.config.data_dvc_path:
+            for data_name, dvc_path in self.config.data_dvc_path.items():
+                if (data_name is not None) & (dvc_path is not None):
+                    MlflowClientWrapper.save_artifact_to_mlruns(dvc_path, artifact_dir_name=f"dvc/data/{data_name}")
+        
+        if self.config.task == Task.SEMANTIC_SEGMENTATION:
+            MlflowClientWrapper.log_metrics_with_array(self.config.result)
+        
+        print('[O] OutputResultTask._do_log_mlflow_task')
